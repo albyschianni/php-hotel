a879075baaf2c3e0d9291bbae31d063ae874756d
albyschianni <alberto.schianni@gmail.com>
2023-01-10 23:47:46 +0100

    prima parte logica foreach

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>Document</title>
+    
+    <style>
+        * {
+            background: lightslategray;
+            color: white;
+        }
+    </style>
+
+    <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+
+        ];
+
+    ?>
+
+</head>
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Nome Hotel</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+    
+        <tbody>
+
+            <?php
+
+                foreach ($hotels as $hotel) {
+
+                // $name = $hotel["name"];
+                // $description = $hotel["description"];
+                // $parking = $hotel["parking"];
+                // $vote = $hotel["vote"];
+                // $distance_to_center = $hotel["distance_to_center"];
+
+                // echo $name . "" . $description . "" . $parking . "" . $vote . "" . $distance_to_center . "" . "<br>";
+                
+                echo "<tr></tr>";
+               
+                foreach ($hotel as $key => $value) {
+                    echo "<td>" . $value . "<td/>";
+                }
+                }
+            ?>
+
+        </tbody>
+    
+    
+    </table>
+
+
+    
+
+
+</body>
+</html>
